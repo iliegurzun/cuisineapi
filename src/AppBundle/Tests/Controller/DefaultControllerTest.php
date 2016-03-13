ffa891a00c962dce75213aa597ab98c1015d654c
Ilie Gurzun <ilie.gurzun@gmail.com>
2016-03-13 16:08:36 +0200

    wrote readme + unit-tests + api endpoints

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -13,7 +13,7 @@ class DefaultControllerTest extends AbstractTest
 {
     private $testingData =
 <<<EOT
-id,created_at,updated_at,box_type,title,slug,short_title,marketing_description,calories_kcal,protein_grams,fat_grams,carbs_grams,bulletpoint1,bulletpoint2,bulletpoint3,recipe_diet_type_id,season,base,protein_source,preparation_time_minutes,shelf_life_days,equipment_needed,origin_country,recipe_cuisine,in_your_box,gousto_reference \n
+id,created_at,updated_at,box_type,title,slug,short_title,marketing_description,calories_kcal,protein_grams,fat_grams,carbs_grams,bulletpoint1,bulletpoint2,bulletpoint3,recipe_diet_type_id,season,base,protein_source,preparation_time_minutes,shelf_life_days,equipment_needed,origin_country,recipe_cuisine,in_your_box,gousto_reference
 1,01-01-2016,01-01-2016,,foo title,foo-title,,,,,,,,,,,,,,,,,,,,
 EOT;
     public function setUp()
